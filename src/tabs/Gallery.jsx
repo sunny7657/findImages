0dd893550c6500cbba3d174e72507b1b4d66dc21
sunny7657 <ksvetlana466@gmail.com>
2023-12-30 18:22:22 +0100

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import * as ImageService from 'service/image-service';
+// import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
